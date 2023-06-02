cc82c18525385db050a68f64c0e4a0736c87f7f0
taufik69 <taufik35-2915@diu.edu.bd>
2023-06-02 21:42:36 +0600

    add navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const custom_body = document.querySelector(".custom_body");
+// const custom_body = document.querySelector(".custom_body");
 
-const submit_btn = document.querySelector(".submit_btn");
-submit_btn.addEventListener("click", () => {
-  custom_body.remove();
-  submit_btn.remove();
+// const submit_btn = document.querySelector(".submit_btn");
+// submit_btn.addEventListener("click", () => {
+//   custom_body.remove();
+//   submit_btn.remove();
+//   setTimeout(() => {
+//     location.reload();
+//   }, 5000);
+// });
+
+// Navagition functionality for login button
+
+const login_btn = document.querySelector("form");
+login_btn.addEventListener("click", () => {
+  console.log("click targeted button");
 });
